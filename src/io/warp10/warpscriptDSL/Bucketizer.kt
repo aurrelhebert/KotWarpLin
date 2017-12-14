4ab0e63702e9197348daf5de7e012a5c0132bfc2
aurrelhebert <aurrelhebert@gmail.com>
2017-12-14 14:44:31 +0100

    "Fix bucketizer"

--- src/io/warp10/warpscriptDSL/Bucketizer.kt
+++ src/io/warp10/warpscriptDSL/Bucketizer.kt
@@ -5,12 +5,31 @@ class Bucketizer: Tag("") {
     fun and() = initTag(BucketizerFunction("bucketizer.and"), {})
     fun count() = initTag(BucketizerFunction("bucketizer.count"), {})
     fun first() = initTag(BucketizerFunction("bucketizer.first"), {})
-    fun join() = initTag(BucketizerFunction("bucketizer.join"), {})
+
+    fun join(value: String): MapperFunction {
+        return initTag(MapperFunction("bucketizer.mean.circular", hashMapOf(0 to "\'$value\'")), {})
+    }
+
     fun last() = initTag(BucketizerFunction("bucketizer.last"), {})
     fun max() = initTag(BucketizerFunction("bucketizer.max"), {})
     fun mean() = initTag(BucketizerFunction("bucketizer.mean"), {})
-    fun meanCircular() = initTag(BucketizerFunction("bucketizer.mean.circular"), {})
-    fun meanCircularExcludeNulls() = initTag(BucketizerFunction("bucketizer.mean.circular.exclude-nulls"), {})
+
+    fun meanCircular(value: Long): MapperFunction {
+        return initTag(MapperFunction("bucketizer.mean.circular", hashMapOf(0 to value.toString())), {})
+    }
+
+    fun meanCircular(value: Double): MapperFunction {
+        return initTag(MapperFunction("bucketizer.mean.circular", hashMapOf(0 to value.toString())), {})
+    }
+
+    fun meanCircularExcludeNulls(value: Long): MapperFunction {
+        return initTag(MapperFunction("bucketizer.mean.circular.exclude-nulls", hashMapOf(0 to value.toString())), {})
+    }
+
+    fun meanCircularExcludeNulls(value: Double): MapperFunction {
+        return initTag(MapperFunction("bucketizer.mean.circular.exclude-nulls", hashMapOf(0 to value.toString())), {})
+    }
+
     fun median() = initTag(BucketizerFunction("bucketizer.median"), {})
     fun min() = initTag(BucketizerFunction("bucketizer.min"), {})
     fun or() = initTag(BucketizerFunction("bucketizer.or"), {})
